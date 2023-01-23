291c972884c523018823490a01864c0cde6d18f2
Samuel.tesema <108014373+Samuel12451@users.noreply.github.com>
2023-01-23 23:31:19 +0300

    Add files via upload
    Adding locally fixed

--- sort.h
+++ sort.h
@@ -1,8 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
+/*Includes*/
+#include <stdio.h>
 
-#include <stdlib.h>
-
+/*Structs*/
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,16 +18,38 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Prototypes */
-void print_array(const int *array, size_t size);
+/*Prototypes*/
+
 void print_list(const listint_t *list);
 
+void print_array(const int *array, size_t size);
+
 void bubble_sort(int *array, size_t size);
+
 void insertion_sort_list(listint_t **list);
+
 void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
 
-void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+
+int split(int *arr, int left, int right, size_t size);
+
+void sort_alg(int *arr, int left, int right, size_t size);
+
+void shell_sort(int *array, size_t size);
+
+void quick_sort_hoare(int *array, size_t size);
+
+void bitonic_sort(int *array, size_t size);
+
+void radix_sort(int *array, size_t size);
+
+void heap_sort(int *array, size_t size);
+
+void merge_sort(int *array, size_t size);
+
 void counting_sort(int *array, size_t size);
-#endif /*SORT_H*/
+
+#endif
