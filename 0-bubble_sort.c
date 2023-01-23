d64b03270a68d611875a97a2d919d09a22dced5b
Samuel.tesema <108014373+Samuel12451@users.noreply.github.com>
2023-01-23 23:51:47 +0300

    Update 0-bubble_sort.c
    fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,36 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order
- * using the Bubble sort algorithm
- * @array: Array to sort
- * @size: size of the array
- *
- * Return: Void
+ * bubble_sort - sorts array in ascending order
+ * @array: array to be sorted
+ * @size: size of array
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, size2 = size;
-	int tmp, flag = 0;
+	size_t i = 0;
+	int holder = NULL;
+	int switches = 0;
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
-	for (i = 0; i < size; i++)
+
+	while (switches >= 0)
 	{
-		for (j = 1; j < size2; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[i + 1] < array[i])
 			{
-				flag = 1;
-				tmp = array[j];
-				array[j] = array[j - 1];
-				array[j - 1] = tmp;
+				holder = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = holder;
+				switches++;
 				print_array(array, size);
 			}
 		}
-		if (flag == 0)
-			break;
-		flag = 0;
-		size2--;
+		if (switches == 0)
+			switches = -1;
+		else
+			switches = 0;
 	}
 }
